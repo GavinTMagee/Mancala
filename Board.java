ce53e0da7971cc30d0b8faae57d3443ca2bb0c76
GavinTMagee <42547621+GavinTMagee@users.noreply.github.com>
2018-12-05 21:26:32 -0700

    Update Board.java
    sketches out Board class

--- Board.java
+++ Board.java
@@ -1,55 +1,88 @@
 public class Board {
-	public int[] board;
-	public int playerScore;
-	public int AIScore;
-	public boolean gameOver;
+	private static final int MANCALA_1 = 7;
+	private static final int MANCALA_2 = 0;
+	
+	private int[] board = new int[14];
+	public boolean currIsPlayer1;
 	
 	public Board() {
-		this.board = new int[14];
 		for (int i = 0; i < 14; i++) {
 			board[i] = 4;
 		}
-		board[0] = 0;
-		board[7] = 0;
-		this.gameOver = false;
-		this.playerScore = 0;
-		this.AIScore = 0;
-		
+		board[MANCALA_1] = 0;
+		board[MANCALA_2] = 0;
+		this.currIsPlayer1 = true;
 	}
 	
-	public void moveIndex(int index){
+	public void moveIndex(int index) {
 		int pieces = board[index];
-		board[index] = 0;
-		for (int i = 0; i < pieces; i++) {
-			board[(index + i) % 14] += 1;
-		}
-		boolean gameOver = true;
-		for (int i = 1; i < 7; i ++) {
-			if (board[i] != 0) {
-				gameOver = false;
+		while (pieces > 0) {
+			index += 1;
+			if (pieces == 1 && board[index] == 0 && currIsPlayer1 && 1 <= index && index <= 6) {//piece capturing opposite
+				board[index] += 1;
+				board[MANCALA_1] += board[14 - index];
+				board[14 - index] = 0;
+				pieces -= 1;
+			} else if (pieces == 1 && currIsPlayer1 && 8 <= index && index <= 13) {//piece landing on what it captures
+				board[MANCALA_1] += board[index] + 1;
+				board[index] = 0;
+				pieces -= 1;
+			} else if (pieces == 1 && board[index] == 0 && !currIsPlayer1 && 8 <= index && index <= 13) {//piece capturing opposite
+				board[index] += 1;
+				board[MANCALA_2] += board[14 - index];
+				board[14 - index] = 0;
+				pieces -= 1;
+			} else if (pieces == 1 && !currIsPlayer1 && 1 <= index && index <= 6) {//piece landing on what it captures
+				board[MANCALA_2] += board[index] + 1;
+				board[index] = 0;
+				pieces -= 1;
+			}else {
+				if (currIsPlayer1 && index != 0) {
+					board[index] += 1;
+					pieces -= 1;
+				}else if (!currIsPlayer1 && index != 7){
+					board[index] += 1;
+					pieces -= 1;
+				}
 			}
 		}
-		if (gameOver) {// AI no more moves
-			for (int i = 8; i < 14; i++) {
-				board[0] += board[i];
-				board[i] = 0;
-			}
-			this.gameOver = true;
+	}
+	
+	private int sumSide(boolean player1Side) {
+		int sum = 0;
+		int shift = (player1Side? 0:7);
+		for(int i = 1; i < 7; i++) {
+			sum += board[i+shift];
 		}
-		gameOver = true;
-		for (int i = 8; i < 14; i++) {//player no more moves
-			if (board[i] != 0) {
-				gameOver = false;
+		return sum;
+	}
+
+	private boolean gameOver() {
+		return sumSide(true)==0 || sumSide(false)==0;
+	}
+
+	// returns true if Player1 has won
+	private boolean gameEnd() {
+		board[MANCALA_1] += sumSide(true);
+		board[MANCALA_2] += sumSide(false);
+		// TODO: better tie system
+		if(board[MANCALA_1] == board[MANCALA_2]) {
+			return (Boolean) null;
+		}
+		return board[MANCALA_1] > board[MANCALA_2];
+	}
+
+	public String toString() {
+		String repr = "     "; // skips mancala
+		for(int i = board.length -1; i > 0; i--) {
+			if(i == MANCALA_1) {
+				repr+="\n|" + String.format("%02d", board[MANCALA_1]) +"|";
+				repr+= new String(new char[(i-1)*4]).replace("\0"," ");
+				repr+=" |" + String.format("%02d", board[MANCALA_2]) +"|\n";
+			} else {
+				repr+=String.format("%02d", board[i])+"  ";
 			}
 		}
-		if (gameOver) {
-			for (int i = 1; i < 7; i++) {
-				board[7] += board[i];
-				board[i] = 0;
-			}
-			this.gameOver = true;
-		}
-		this.playerScore = board[0];
-		this.AIScore = board[7];
+		return repr;
 	}
 }
